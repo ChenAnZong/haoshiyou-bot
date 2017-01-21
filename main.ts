da107e10182383683782cb3b371b5530f25d03c4
Zainan Victor Zhou <xinbenlv@gmail.com>
2017-01-21 15:49:03 -0800

    Auto add group

--- main.ts
+++ main.ts
@@ -1,13 +1,19 @@
-import {Wechaty, Room, Contact} from "wechaty";
+import {Wechaty, Room} from "wechaty";
 import {HsyBotLogger} from "./logger";
 import {FriendRequest} from "wechaty/dist/src/friend-request";
 
-console.log('XXX DEBUG Start!');
+console.log('XXX DEBUG Start 2!');
 
 const bot = Wechaty.instance();
-const hsyGreetingsMsg = '你好，谢谢你加我们群，请问你要在哪个区域（请告诉我City）找房子或者室友？' +
-    '我们是按照区域分群的。我拉你入群？';
-
+const hsyGreetingsMsg =
+    `你好，谢谢你加我们群，请问你要在哪个区域找房子或者室友？\n` +
+    `我们是按照区域分群的。我拉你入群：\n` +
+    `  【南湾西】包含Palo Alto，Stanford, Mountain View，Sunnyvale，Cupertino一带；\n` +
+    `  【南湾东】包含San Jose，Santa Clara，Milpitas一带；\n` +
+    `  【东湾】湾东边Milpitas以北，包括Fremont，Hayward，Berkeley等；\n` +
+    `  【中半岛】Redwood以北，San Francisco以南；\n` +
+    `  【三番】旧金山(San Francisco)城里，含South San Francisco；\n`+
+    `请回复要加哪个群，例如： 南湾西`;
 bot
     .on('scan', (url, code) => {
       HsyBotLogger.logDebug(`Please scan the QR code for URL ${url}.`);
@@ -41,105 +47,46 @@ bot
       const contact = m.from();
       const content = m.content();
       const room = m.room();
-
-      if (room) {
-        console.log(`Room: ${room.topic()} Contact: ${contact.name()} Content: ${content}`)
-      } else {
-        console.log(`Contact: ${contact.name()} Content: ${content}`)
-      }
-
       if (m.self()) {
         return; // Early return for talking to myself.
       }
 
-      if (/hello|你好/.test(content)) {
-        m.say(hsyGreetingsMsg);
-      }
+      if (m.rawObj['MMIsChatRoom'] == false) { // only to me
+        console.log('Talking to 好室友 admin');
+        let groupToAdd = null;
 
-      if (/求加/.test(content)) {
-        if (/南湾/.test(content)) {
-          if (/南湾西|Mountain View|mtv|sv|Sunnyvale|Palo Alto|Stanford|Facebook|Google/.test(content)) {
-            m.say('好的，我这就拉你进群。');
-
-            console.log(`Adding group...`);
-            let keyroom = await Room.find({topic: /好室友.*南湾西/});
-
-            console.log(`Found group...`);
-            await downsizeRoom(keyroom);
-            if (keyroom) {
-              console.log(`Adding the contact: ${contact}`);
-              await keyroom.add(contact);
-              console.log(`Added the contact: ${contact}`);
-              await keyroom.say("欢迎新群友，", contact);
-            } else {
-              console.log(`Didn't found 南湾西.`);
-            }
-          } else if (/南湾东|Milpitas|San Jose|Santa Clara/.test(content)) {
-            m.say('好的，我这就拉你进群。');
-            let keyroom = await Room.find({topic: /好室友.*南湾东/});
-            if (keyroom) {
-              console.log(`Adding the contact: ${contact}`);
-              await keyroom.add(contact);
-              console.log(`Added the contact: ${contact}`);
-              await keyroom.say("欢迎新群友，", contact);
-            }
-          } else {
-            m.say('南湾分为南湾东和南湾西，你想加哪个区域？以Santa Clara City为界。' +
-                '例如Palo Alto, Mountain View, Sunnyvale, Cuptertino都属于南湾西，' +
-                'San Jose, Milptas, Santa Clara都属于南湾东。');
-          }
+        if (/南湾西|Mountain View|mtv|sv|Sunnyvale|Palo Alto|Stanford|Facebook|Google/.test(content)) {
+          groupToAdd = "南湾西";
+        } else if (/南湾东|Milpitas|San Jose|Santa Clara/.test(content)) {
+          groupToAdd = "南湾东";
         } else if (/东湾|奥克兰|伯克利/.test(content)) {
-            m.say('好的，我这就拉你进群。');
-            let keyroom = await Room.find({topic: /好室友.*东湾/});
-            if (keyroom) {
-              console.log(`Adding the contact: ${contact}`);
-              await keyroom.add(contact);
-              console.log(`Added the contact: ${contact}`);
-              await keyroom.say("欢迎新群友，", contact);
-            }
+          groupToAdd = "东湾";
         } else if (/(中)半岛/.test(content)) {
-          m.say('好的，我这就拉你进群。');
-          let keyroom = await Room.find({topic: /好室友.*中半岛/});
-          if (keyroom) {
-            console.log(`Adding the contact: ${contact}`);
-            await keyroom.add(contact);
-            console.log(`Added the contact: ${contact}`);
-            await keyroom.say("欢迎新群友，", contact);
-          }
+          groupToAdd = "中半岛";
         } else if (/旧金山|三番|San Francisco/.test(content)) {
-          m.say('好的，我这就拉你进群。');
-          let keyroom = await Room.find({topic: /好室友.*旧金山/});
+          groupToAdd = "三番";
+        } else if (/testbotgroup/.test(content)) {
+          groupToAdd = "testgroup"
+        }
+
+        if (groupToAdd == null) { // found no valid group
+          m.say(hsyGreetingsMsg);
+        } else {
+          console.log(`Start to add ${contact} to room ${groupToAdd}.`);
+          m.say(`好的，你要加${groupToAdd}的群对吧，我这就拉你进群。`);
+          let typeRegEx = new RegExp(`好室友.*` + groupToAdd);
+          let keyroom = await Room.find({topic: typeRegEx});
           if (keyroom) {
-            console.log(`Adding the contact: ${contact}`);
             await keyroom.add(contact);
             console.log(`Added the contact: ${contact}`);
-            await keyroom.say("欢迎新群友，", contact);
+            await m.say("欢迎欢迎，", contact);
+          } else {
+            m.say(`囧...加群失败，请联系群主周载南(微信号:xinbenlv)。`);
+            console.log(`Didn't found ${groupToAdd}.`);
           }
         }
       }
 
-      if (/testbotgroup/.test(content)) {
-        console.log('Received a request to join testbotgroup');
-        m.say('好的，我这就拉你进群。');
-        console.log('Looking for testbotgroup');
-        let keyroom = await Room.find({topic: "testbotgroup"});
-
-        console.log('Found');
-        if (keyroom) {
-          console.log(`Adding the contact: ${contact}`);
-          await keyroom.add(contact);
-          console.log(`Added the contact: ${contact}`);
-          await keyroom.say("欢迎新群友，", contact);
-        }
-      }
-
-      if (/out/.test(content)) {
-        let keyroom = await Room.find({topic: "test"});
-        if (keyroom) {
-          await keyroom.say("Remove from the room", contact);
-          await keyroom.del(contact)
-        }
-      }
     })
 
     .init();
