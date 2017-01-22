653cb2546987a4a8b95cb6eb44e1677c7acb658f
Zainan Victor Zhou <xinbenlv@gmail.com>
2017-01-21 17:13:28 -0800

    Complete add group and downsize feature

--- main.ts
+++ main.ts
@@ -1,10 +1,11 @@
-import {Wechaty, Room} from "wechaty";
+import {Wechaty, Room, Contact} from "wechaty";
 import {HsyBotLogger} from "./logger";
 import {FriendRequest} from "wechaty/dist/src/friend-request";
 
-console.log('XXX DEBUG Start 2!');
+console.log('XXX DEBUG Start v 3!');
 
 const bot = Wechaty.instance();
+const groupDownSizeTarget = 480;
 const hsyGreetingsMsg =
     `你好，谢谢你加我们群，请问你要在哪个区域找房子或者室友？\n` +
     `我们是按照区域分群的。我拉你入群：\n` +
@@ -13,7 +14,10 @@ const hsyGreetingsMsg =
     `  【东湾】湾东边Milpitas以北，包括Fremont，Hayward，Berkeley等；\n` +
     `  【中半岛】Redwood以北，San Francisco以南；\n` +
     `  【三番】旧金山(San Francisco)城里，含South San Francisco；\n`+
-    `请回复要加哪个群，例如： 南湾西`;
+    `  【短租】如果你希望在湾区任意地方内进行3个月以内短租（出租和求租），请加短租群；\n`+
+    `请回复要加哪个群，例如： 南湾西\n` +
+    `另外如果你在我们群里找到了室友或者房子，欢迎加入我们的【好室友】"老友群"，闲聊~，` +
+    `详情请私信群主周载南(wechat:xinbenlv)或者入口群的管理员们`;
 bot
     .on('scan', (url, code) => {
       HsyBotLogger.logDebug(`Please scan the QR code for URL ${url}.`);
@@ -51,7 +55,53 @@ bot
         return; // Early return for talking to myself.
       }
 
-      if (m.rawObj['MMIsChatRoom'] == false) { // only to me
+      function maybeDownsizeKeyRoom(keyroom: Room) {
+        // keyroom.memberList().forEach((c:Contact) => console.log(c['rawObj']['DisplayName'] || `无法找到群昵称,用户名称为${c.name()}`));
+        if (keyroom.memberList().length >= groupDownSizeTarget) {
+          keyroom.say(
+              `亲爱的各位好室友租房群的群友们，现在群快满了，清理一批群友给新朋友们腾位置。\n` +
+              `我们主要清理两类朋友：\n` +
+              `  1. 没有按照改群昵称的朋友，如果你的群昵称不是以'招'、'求'、'介'开头，那么你可能会被优先清理；` +
+              `  2. 如果你的入群时间比较长，那么我们会请你优先离群，把空位流动起来（可以重新回来）；` +
+              `若仍有需求，欢迎私信好室友小助手（微信号：haoshiyou-admin）重新加群哈~`);
+          let potentialRotationList = [];
+          let noGroupNickNames = [];
+          let cList:Contact[] = keyroom.memberList();
+          let shouldRemoveSize = cList.length - groupDownSizeTarget;
+          let shouldRemoveList = [];
+          for (let i = 0; i < keyroom.memberList().length - 30/*never remove last 30 users*/; i++) {
+            let c:Contact = cList[i];
+            if (c.self()) continue; // never does anything with haoshiyou-admin itself.
+            let groupNickName = c['rawObj']['DisplayName'];
+            if (/^(管|介|群主)-/.test(groupNickName)) {
+              // pass, never remove
+            } else if (/^(招|求)租/.test(groupNickName)) {
+              // good format, but need to rotate
+              potentialRotationList.push(c);
+            } else {
+              noGroupNickNames.push(c);
+            }
+            if (noGroupNickNames.length >= shouldRemoveSize) {
+              shouldRemoveList = noGroupNickNames;
+              break;
+            } else if (noGroupNickNames.length + potentialRotationList.length >= shouldRemoveSize) {
+              shouldRemoveList = noGroupNickNames
+                  .concat(potentialRotationList.slice(0,
+                      shouldRemoveSize - noGroupNickNames.length));
+              break;
+            }
+          }
+          shouldRemoveList.forEach((c:Contact) => {
+            console.log(`INFO Deleting contact ${c.name()} from group ${keyroom.topic()}`);
+            keyroom.del(c); // a promise, but we won't wait.
+          });
+        } else {
+          console.log(`Group Size of ${keyroom.topic()} is ` +
+              `still good (${keyroom.memberList().length}).`)
+        }
+      }
+
+      if (m.rawObj['MMIsChatRoom'] == false || /好室友.*入口群/.test(m.room().topic())) { // only to me or entry group
         console.log('Talking to 好室友 admin');
         let groupToAdd = null;
 
@@ -65,6 +115,8 @@ bot
           groupToAdd = "中半岛";
         } else if (/旧金山|三番|San Francisco/.test(content)) {
           groupToAdd = "三番";
+        } else if (/短租/.test(content)) {
+          groupToAdd = "短租";
         } else if (/testbotgroup/.test(content)) {
           groupToAdd = "testgroup"
         }
@@ -78,7 +130,7 @@ bot
           let keyroom = await Room.find({topic: typeRegEx});
           if (keyroom) {
             await keyroom.add(contact);
-            console.log(`Added the contact: ${contact}`);
+            maybeDownsizeKeyRoom(keyroom);
             await m.say("欢迎欢迎，", contact);
           } else {
             m.say(`囧...加群失败，请联系群主周载南(微信号:xinbenlv)。`);
@@ -90,8 +142,3 @@ bot
     })
 
     .init();
-
-let downsizeRoom = async function(room:Room) {
-  let contacts = room.memberList();
-  console.log(JSON.stringify(contacts));
-}
