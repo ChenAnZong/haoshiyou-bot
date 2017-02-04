49082c7c5ff29bd3787a3219f41ef7b7b07b0e88
Zainan Victor Zhou <xinbenlv@gmail.com>
2017-02-03 22:30:46 -0800

    Update try-loopback.ts

--- try-loopback.ts
+++ try-loopback.ts
@@ -1,5 +1,5 @@
 import {HsyListing} from "./loopbacksdk/models/HsyListing";
-let config = {
+const config = {
   "loopback": {
     "http://localhost:3000": {
       "__domain": {
@@ -15,31 +15,54 @@ let config = {
     }
   },
 };
+const request = require('request');
+const promise = require('bluebird');
+const purest = require('purest')({request, promise});
+const loopback = purest({provider: 'loopback', config});
 
-console.log('Connect to LoopBack Server');
-let request = require('request');
-let promise = require('bluebird');
-let purest = require('purest')({request, promise});
-let loopback = purest({provider: 'loopback', config});
+class LoopbackQuerier {
+  public async getHysListing():Promise<Object> {
+    console.log('Connect to LoopBack Server');
 
-let req = loopback
-    .get('HsyListings')
-    .request();
+    let req = loopback
+        .get('HsyListings')
+        .request();
+    let result = await req
+        .catch((err) => {
+          console.log(JSON.stringify(err));
+        });
+    let listings:HsyListing[] = result[0].body;
+    console.log(JSON.stringify(listings));
+    return result;
+  }
 
-req
-    .catch((err) => {
-      console.log(JSON.stringify(err));
-    })
-    .then((result) => {
-      let listings:HsyListing[] = result[0].body;
-      console.log(JSON.stringify(listings));
-    });
+  public async setHsyListing(listing) {
 
-let listing = new HsyListing();
-listing.ownerId = 'some_ownerId';
-listing.uid = 'some_uid';
-listing.title = 'some title';
-listing.content = 'some content';
-listing.lastUpdated = new Date();
+    let req = loopback.put('HsyListings')
+        .json(listing)
+        .request();
 
+    let result = await req
+        .catch((err) => {
+          console.log(JSON.stringify(err));
+        });
+    console.log('Successfully!');
+    let listings:HsyListing[] = result[0].body;
+    console.log(JSON.stringify(listings));
+    return result;
+  }
+
+  public static async main() {
+    let listing = new HsyListing();
+    listing.ownerId = 'some_ownerId_1';
+    listing.uid = 'some_uid_1';
+    listing.title = 'some title';
+    listing.content = 'some content';
+    listing.lastUpdated = new Date();
+    let q:LoopbackQuerier = new LoopbackQuerier();
+    await q.setHsyListing(listing);
+  }
+}
+
+LoopbackQuerier.main();
 
