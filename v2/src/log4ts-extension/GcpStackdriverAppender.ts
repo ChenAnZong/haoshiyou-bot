cc919c147618496036a60f1ea65db85abbaac665
Zainan Victor Zhou <xinbenlv@gmail.com>
2017-12-17 23:10:44 -0800

    Implemented v2 listening message

---
+++ v2/src/log4ts-extension/GcpStackdriverAppender.ts
@@ -0,0 +1,76 @@
+import { IAppender } from "log4ts/build/IAppender";
+import { LogEntry } from "log4ts/build/LogEntry";
+import BaseAppender from "log4ts/build/appenders/BaseAppender";
+import {LogLevel} from 'log4ts/build/LogLevel';
+const os = require('os');
+// Imports the Google Cloud client library
+const GcpSdLogging = require('@google-cloud/logging');
+
+// Your Google Cloud Platform project ID
+const projectId = 'haoshiyou-prod';
+
+// Creates a client
+const logging = new GcpSdLogging({
+  projectId: projectId,
+
+  // Ask xinbenlv@github for keyfile if you need it. It comes from Google Cloud Platform xinbenlv@gmail.com account
+  // for the Service Account: default-owner@haoshiyou-prod.iam.gserviceaccount.com
+  keyFilename: './.credentials/haoshiyou-prod-dc95d5f118ae.json'
+});
+
+
+// DEFAULT	(0) The log entry has no assigned severity level.
+// DEBUG	(100) Debug or trace information.
+// INFO	(200) Routine information, such as ongoing status or performance.
+// NOTICE	(300) Normal but significant events, such as start up, shut down, or a configuration change.
+// WARNING	(400) Warning events might cause problems.
+// ERROR	(500) Error events are likely to cause problems.
+// CRITICAL	(600) Critical events cause more severe problems or outages.
+// ALERT	(700) A person must take an action immediately.
+// EMERGENCY	(800) One or more systems are unusable.
+
+const mapping = function (logLevel:number) {
+  if (logLevel == LogLevel.TRACE) return 0;
+  else if (logLevel == LogLevel.DEBUG) return 100;
+  else if (logLevel == LogLevel.INFO) return 200;
+  else if (logLevel == LogLevel.WARN) return 400;
+  else if (logLevel == LogLevel.ERROR) return 500;
+};
+
+// The name of the log to write to
+const logName = 'haoshiyou-bot';
+// Selects the log to write to
+const log = logging.log(logName);
+
+const startTimestamp = new Date();
+
+export default class GcpStackdriverAppender extends BaseAppender implements IAppender {
+  append(entry: LogEntry): void {
+    // The metadata associated with the entry
+    const metadata = {
+      resource: {
+        type: "logging_sink",
+        labels: {
+          project_id: `haoshiyou-prod`,
+          name: `haoshiyou-bot-v2-dev`
+        }
+      },
+      timestamp: entry.time,
+      severity: mapping(entry.level),
+      labels: {
+        hostname: `${os.hostname()}`,
+        start_time: `${startTimestamp.toISOString()}`,
+        tag: `${entry.tag}`
+      }
+    };
+
+    // Prepares a log entry
+    const sdLogEntry = log.entry(metadata, entry.message);
+
+    // Writes the log entry
+    log.write(sdLogEntry);
+  }
+  clear(): void {
+    throw new Error(`The method of clear is not implemented`);
+  }
+}
--- v2/src/main.ts
+++ v2/src/main.ts
@@ -1,8 +1,131 @@
-const { Wechaty } = require('wechaty'); // import { Wechaty } from 'wechaty'
 import {Message, Contact, Room} from "wechaty";
+import {Logger, LoggerConfig} from "log4ts";
+const PubSub = require(`@google-cloud/pubsub`);
+// Imports the Google Cloud client library
+const GcpSdLogging = require('@google-cloud/logging');
 
+// Your Google Cloud Platform project ID
+const projectId = 'haoshiyou-dev';
+const keyFile = './.credentials/Haoshiyou-Dev-94b0d8e19b61.json';
+const metadataResource = {
+    type: "logging_sink",
+    labels: {
+      project_id: projectId,
+      name: `haoshiyou-bot-v2-dev`
+    }
+};
+// Creates a client
+const logging = new GcpSdLogging({
+  projectId: projectId,
+  keyFilename: keyFile
+});
+
+// Instantiates a client
+const pubsub = PubSub({
+  projectId: projectId,
+  keyFilename: keyFile
+});
+
+// The name of the log to write to
+const logName = 'haoshiyou-bot';
+// Selects the log to write to
+const log = logging.log(logName);
+
+function publishMessageToPubSub (topicName:string, data:object) {
+
+  // References an existing topic, e.g. "my-topic"
+  const topic = pubsub.topic(topicName);
+
+  // Create a publisher for the topic (which can include additional batching configuration)
+  const publisher = topic.publisher();
+
+  // Publishes the message as a string, e.g. "Hello, world!" or JSON.stringify(someObject)
+  const dataBuffer = Buffer.from(JSON.stringify(data));
+  return publisher.publish(dataBuffer)
+      .then((results:string[]) => {
+        const messageId = results[0];
+
+        console.log(`Message ${messageId} published.`);
+
+        return messageId;
+      }).catch((e:any) => {
+        console.error(e);
+      });
+}
+
+function sendToStackDriver(label:string, data:any) {
+  // The metadata associated with the entry
+  let labels:any = {};
+  labels.tag = label;
+  if (data.from_obj) {
+    labels.from = data.from_obj.name;
+  }
+  if (data.to_obj) {
+    labels.to = data.to_obj.name;
+  }
+  if (data.room_obj) {
+    labels.room = data.room_obj.topic;
+  }
+  const metadata = {
+    resource: metadataResource,
+    severity: 200, // Info
+    labels: labels
+  };
+
+  // Prepares a log entry
+  const sdLogEntry = log.entry(metadata, data);
+
+  // Writes the log entry
+  log.write(sdLogEntry);
+}
+
+let storeWeChatyMessage = function(message:Message) {
+  let logObj = JSON.parse(JSON.stringify(message.obj));
+  if (message.from() != null) {
+    logObj.from_obj = (message.from() as Contact).obj;
+  }
+  if (message.to() != null) {
+    logObj.to_obj = (message.to() as Contact).obj;
+  }
+  if (message.room() != null) {
+    let room = message.room() as Room;
+    logObj.room_obj = {
+      topic: room.topic(),
+      memberListSize: room.memberList().length,
+      id: room.id,
+      senderRoomAlias: room.roomAlias(message.from())
+    };
+  }
+
+  sendToStackDriver('msg-all', logObj);
+  publishMessageToPubSub('msg-all', logObj);
+};
+
+const { Wechaty } = require('wechaty'); // import { Wechaty } from 'wechaty'
 Wechaty.instance() // Singleton
-    .on('scan', (url:string, code:any) => console.log(`Scan QR Code to login: ${code}\n${url}`))
+    .on('scan', async (url:string, code:any) => {
+      const logger = Logger.getLogger(`scan`);
+      switch (code) {
+        case 408:
+          let loginUrl = url.replace('qrcode', 'l');
+          require('qrcode-terminal').generate(loginUrl);
+          break;
+        case 200:
+          logger.debug(`WeChaty Scan 200 login confirmed`);
+          break;
+        case 201:
+          logger.debug(`WeChaty Scan 201 scanned, wait for confirm`);
+          break;
+        case 0:
+          logger.debug(`WeChaty Scan 0 init`);
+          break;
+        default:
+          logger.debug(`WeChaty Scan Other code: ${code}`);
+      }
+    })
     .on('login',       (user:Contact) => console.log(`User ${user} logined`))
-    .on('message',  (message:Message) => console.log(`Message: ${message}`))
+    .on('message',  (message:Message) => {
+      console.log(message);
+      storeWeChatyMessage(message);
+    })
     .start();
